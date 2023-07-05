8d027b9856beec8a6c26caffd1c374e01f8940bf
ATOMworkplace <114564628+ATOMworkplace@users.noreply.github.com>
2023-07-05 07:56:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ const html=`<!DOCTYPE html>
             </div>
         <div class="location">
             <center>
-            <img class="location-icon" src="images/pin.png"><br>
+            <img class="location-icon" src="https://finbarhussey.com/whats-new/tag/gifs"><br>
             </center>
             <span name="location-text" class="location-text">Location</span><br>
             <center>            
